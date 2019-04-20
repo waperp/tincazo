5ad6525b562e5e650cea57efdbd26fcfb36c03f5
miguel <miguelhangelh@hotmail.com>
2019-04-20 11:47:17 -0400

    up

--- resources/views/layout/welcome.blade.php
+++ resources/views/layout/welcome.blade.php
@@ -253,6 +253,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script src="/assets/vendor/jpreloader2/js/jpreloader.js" type="text/javascript">
 </script>
   <script src="/js/sum.js" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+    if (window.location.hash && window.location.hash == '#_=_') {
+        if (window.history && history.pushState) {
+            window.history.pushState("", document.title, window.location.pathname);
+        } else {
+            // Prevent scrolling by storing the page's current scroll offset
+            var scroll = {
+                top: document.body.scrollTop,
+                left: document.body.scrollLeft
+            };
+            window.location.hash = '';
+            // Restore the scroll offset, should be flicker free
+            document.body.scrollTop = scroll.top;
+            document.body.scrollLeft = scroll.left;
+        }
+    }
+</script>
 {{-- <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
   
 </script>

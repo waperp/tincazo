0d783ba8e10cb3a67b60c12e5b64ab5daecfaae9
miguel <miguelhangelh@hotmail.com>
2019-04-20 11:50:21 -0400

    Update welcome.blade.php

--- resources/views/layout/welcome.blade.php
+++ resources/views/layout/welcome.blade.php
@@ -256,21 +256,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 
 <script type="text/javascript">
-    if (window.location.hash && window.location.hash == '#_=_') {
-        if (window.history && history.pushState) {
-            window.history.pushState("", document.title, window.location.pathname);
-        } else {
-            // Prevent scrolling by storing the page's current scroll offset
-            var scroll = {
-                top: document.body.scrollTop,
-                left: document.body.scrollLeft
-            };
-            window.location.hash = '';
-            // Restore the scroll offset, should be flicker free
-            document.body.scrollTop = scroll.top;
-            document.body.scrollLeft = scroll.left;
-        }
-    }
+   // Remove the ugly Facebook appended hash
+// <https://github.com/jaredhanson/passport-facebook/issues/12>
+if (window.location.hash && window.location.hash === "#_=_") {
+  // If you are not using Modernizr, then the alternative is:
+  //   `if (window.history && history.replaceState) {`
+  if (Modernizr.history) {
+    window.history.replaceState("", document.title, window.location.pathname);
+  } else {
+    // Prevent scrolling by storing the page's current scroll offset
+    var scroll = {
+      top: document.body.scrollTop,
+      left: document.body.scrollLeft
+    };
+    window.location.hash = "";
+    // Restore the scroll offset, should be flicker free
+    document.body.scrollTop = scroll.top;
+    document.body.scrollLeft = scroll.left;
+  }
+}
 </script>
 {{-- <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
   

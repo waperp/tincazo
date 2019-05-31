6c349501df86e4025b3f4a291ae784dd4dec26ba
miguel <miguelhangelh@hotmail.com>
2019-05-31 14:01:39 -0400

    up

--- resources/views/emails/sendValidateMail.blade.php
+++ resources/views/emails/sendValidateMail.blade.php
@@ -10,10 +10,30 @@
 
 Has click en el enlace para proceder a cambiar tu contraseña.
 
-@component('mail::button', 
-['url' => 'http://tincazo.com/reset/password?utm_ref='.$data->secusrtmail."&utm_source=".encrypt($data->secusricode)."&utm_value=".encrypt($data->secpininump)])
-Has click Aqui
-@endcomponent
+{{-- @component('button', 
+['url' => ])
+
+@endcomponent --}}
+
+<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td align="center">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                <tr>
+                    <td align="center">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation">
+                            <tr>
+                                <td>
+                                    <a href="http://tincazo.com/reset/password?utm_ref={{ $data->secusrtmail }}&utm_source={{encrypt( $data->secusricode )}}&utm_value={{encrypt( $data->secpininump )}}" class="button button-primary" target="_blank">Has click Aqui</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 
 
 Gracias,<br>

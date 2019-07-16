b470ceb2cacfbe2ed96364256e33a8182a1a8db4
Miguel Angel <miguelwisse@wisse.tech>
2019-07-16 15:29:31 -0400

    Update matches.blade.php

--- resources/views/matches.blade.php
+++ resources/views/matches.blade.php
@@ -38,36 +38,11 @@
             <header class="card__header card__header--has-filter">
                 <h4>PARTIDOS </h4>
                 
-                        <div class="row" style="margin-bottom: 8px">
-                           
-                            <div class="col-md-8">
-                                <div class="input-group-btn input-group-btn-sm">
-                                    <div class="input-group input-group-sm" style="width: 100%">
-                                        <div class="input-group-btn input-group-btn-sm">
-                                            <div class="btn-group btn-group-sm">
-                                                <button title="Refrescar Tincazos" type="button"
-                                                    class="btn btn-primary-inverse refresh-button">
-                                                    <i class="fa fa-refresh"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <input type="search" class="form-control buscar input-sm"
-                                            placeholder="Busca por equipos">
-                                        <div class="input-group-btn">
-                                            <button title="Buscar Tincazos" type="button" id="shearh-tincazos"
-                                                class="btn btn-primary"><i class="glyphicon glyphicon-search"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                  
             </header>
             <div class="tab-content">
                 
                 <div class="card__content" style="height: 600px;overflow-y:  auto;">
-                    <div class="row">
+                    <div class="row" style="margin-bottom: 15px">
                             <div class="col-md-4">
                                     <div class="input-group input-group-sm date" id="datetimepicker-toufixdplay-matches-full">
                                         <input class="form-control c"  placeholder="Ingrese su fecha inicio"
@@ -78,6 +53,27 @@
                                         </span>
                                     </div>
                                 </div>
+                                <div class="col-md-8">
+                                        <div class="input-group-btn input-group-btn-sm">
+                                            <div class="input-group input-group-sm" style="width: 100%">
+                                                <div class="input-group-btn input-group-btn-sm">
+                                                    <div class="btn-group btn-group-sm">
+                                                        <button title="Refrescar Tincazos" type="button"
+                                                            class="btn btn-primary-inverse refresh-button">
+                                                            <i class="fa fa-refresh"></i>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                                <input type="search" class="form-control buscar input-sm"
+                                                    placeholder="Busca por equipos">
+                                                <div class="input-group-btn">
+                                                    <button title="Buscar Tincazos" type="button" id="shearh-tincazos"
+                                                        class="btn btn-primary"><i class="glyphicon glyphicon-search"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                     </div>
                         
                     <div class="game-result" id="matches_all">

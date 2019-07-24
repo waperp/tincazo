d878e461232545693d17cbf1a9399fd6784516c7
miguelwisse <miguelhangelh@hotmail.com>
2019-07-24 15:52:51 -0400

    up

--- public/js/matches.js
+++ public/js/matches.js
@@ -1,5 +1,5 @@
-var touinfscode_static ;
-$(document).ready(function () {
+var touinfscode_static;
+$(document).ready(function() {
     $('#datetimepicker-toufixdplay-matches-full').datetimepicker({
         locale: 'es',
         format: 'DD-MM-YYYY',
@@ -8,16 +8,22 @@ $(document).ready(function () {
         debugger
         matches_all(touinfscode_static);
     });
+    var touinfscode = $("#post-torneos li:first").data('touinfscode');
+    if (touinfscode) {
+        touinfscode_static = touinfscode;
+        matches_all(touinfscode_static);
+    }
+    debugger
 });
 $(".refresh-button").on('click', function() {
     matches_all(touinfscode_static);
 });
 $("#shearh-matches").on('click', function() {
     matches_all(touinfscode_static);
- });
+});
 
 function matches_all(touinfscode) {
-    $('body').Wload({text:' Cargando'})
+    $('body').Wload({ text: ' Cargando' })
     var touteatname = $(".buscar").val();
     touinfscode_static = touinfscode;
     var toufixdplay = $('#datetimepicker-toufixdplay-matches-full').data('DateTimePicker').date().format('YYYY-MM-DD');
@@ -31,74 +37,86 @@ function matches_all(touinfscode) {
             toufixdplay: toufixdplay,
             touteatname: touteatname
         },
-        success: function (data) {
-           
+        success: function(data) {
+
             $('#matches_all').empty();
-            $.each(data, function (i, item) {
+            $.each(data, function(i, item) {
                 // Create with DOM
-                var spacer = $('<div>',{class:'spacer'})
-                var progress = $('<div >', { class: 'progress', style: "height:15px; margin-bottom:0px;top: 0px; ", 
-                html: $('<div class="'+ constatdesc_matches(item)+'">'+item.constatdesc+'</div>') });
+                var spacer = $('<div>', { class: 'spacer' })
+                var progress = $('<div >', {
+                    class: 'progress',
+                    style: "height:15px; margin-bottom:0px;top: 0px; ",
+                    html: $('<div class="' + constatdesc_matches(item) + '">' + item.constatdesc + '</div>')
+                });
                 var span_col_1 = $('<div>', { class: 'col-md-5', style: 'text-align: left', html: $('<span>', { class: 'game-result__league', text: moment(item.toufixdplay).lang('es').format('dddd DD [de] MMMM') }) });
                 var span_col_2 = $('<div>', { class: 'col-md-2', html: [progress], });
                 var span_col_3 = $('<div>', { class: 'col-md-5', html: $('<time>', { class: 'game-result__date', text: moment(item.toufixdplay + " " + item.toufixthour).lang('es').format('HH:mm A') }) });
-                var header = $('<header>',{  style:'margin-top:0px;margin-bottom:0px',class: 'game-result__header game-result__header--alt', html: [span_col_1, span_col_2, span_col_3], });
-                var game_result_team_first = $('<div>',{class:'game-result__team game-result__team--first',
+                var header = $('<header>', { style: 'margin-top:0px;margin-bottom:0px', class: 'game-result__header game-result__header--alt', html: [span_col_1, span_col_2, span_col_3], });
+                var game_result_team_first = $('<div>', {
+                    class: 'game-result__team game-result__team--first',
                     html: [
-                        $('<figure>',{class:'game-result__team-logo',
-                            html:$('<img>',{
-                                src: 'images/'+item.touteavimgt
+                        $('<figure>', {
+                            class: 'game-result__team-logo',
+                            html: $('<img>', {
+                                src: 'images/' + item.touteavimgt
                             })
                         }),
-                        $('<div>',{class:'game-result__team-info',
-                            html:$('<h5>',{
-                                class:'game-result__team-name',
+                        $('<div>', {
+                            class: 'game-result__team-info',
+                            html: $('<h5>', {
+                                class: 'game-result__team-name',
                                 text: item.touteatname
                             })
                         })
                     ]
                 });
-                var game_result_team_score = $('<div>',{class:'game-result__score-wrap',
-                    html: $('<div>',{
+                var game_result_team_score = $('<div>', {
+                    class: 'game-result__score-wrap',
+                    html: $('<div>', {
                         class: 'game-result__score game-result__score--lg',
-                        html:[
-                            $('<span>',{
+                        html: [
+                            $('<span>', {
                                 class: matches_score_type_team1(item),
                                 text: item.toufixsscr1
                             }),
-                            $('<span>',{
+                            $('<span>', {
                                 class: 'game-result__score-dash',
-                                text:'-'
+                                text: '-'
                             }),
-                            $('<span>',{
+                            $('<span>', {
                                 class: matches_score_type_team2(item),
                                 text: item.toufixsscr2
                             })
                         ]
                     })
                 });
-                var game_result_team_second = $('<div>',{class:'game-result__team game-result__team--second',
+                var game_result_team_second = $('<div>', {
+                    class: 'game-result__team game-result__team--second',
                     html: [
-                        $('<figure>',{class:'game-result__team-logo',
-                            html:$('<img>',{
-                                src: 'images/'+item.touteavimgt2
+                        $('<figure>', {
+                            class: 'game-result__team-logo',
+                            html: $('<img>', {
+                                src: 'images/' + item.touteavimgt2
                             })
                         }),
-                        $('<div>',{class:'game-result__team-info',
-                            html:$('<h5>',{
-                                class:'game-result__team-name',
+                        $('<div>', {
+                            class: 'game-result__team-info',
+                            html: $('<h5>', {
+                                class: 'game-result__team-name',
                                 text: item.touteatname2
                             })
                         })
                     ]
                 });
-                var game_result = $('<div>',{class:'game-result__content',
-                style: 'margin: 0 0 5px 0;',
-                html: [game_result_team_first,game_result_team_score,game_result_team_second]
+                var game_result = $('<div>', {
+                    class: 'game-result__content',
+                    style: 'margin: 0 0 5px 0;',
+                    html: [game_result_team_first, game_result_team_score, game_result_team_second]
                 });
 
-                var section = $('<section>', { class: 'game-result__section pt-0', 
-                    html: [header,game_result]
+                var section = $('<section>', {
+                    class: 'game-result__section pt-0',
+                    html: [header, game_result]
                 })
                 $("#matches_all").append(section);
             });
@@ -124,58 +142,62 @@ function matches_all(touinfscode) {
             //     // }
         },
         complete: function(data) {
-            $('body').Wload('hide',{time:1000})
+            $('body').Wload('hide', { time: 1000 })
         }
     });
 }
-function load_matches(touinfscode,elm){
+
+function load_matches(touinfscode, elm) {
     debugger
-    touinfscode_static = touinfscode; 
+    touinfscode_static = touinfscode;
     $('li.posts__item').removeClass('post__items__custom__selected');
     $('li > figure > img.post__items__custom__img').removeClass('post__items__custom__img__selected');
     $(elm).addClass('post__items__custom__selected');
     var img = $(elm).children().children()[0];
     $(img).removeClass('post__items__custom__img__selected');
     $(img).addClass('post__items__custom__img__selected');
+
     matches_all(touinfscode);
 }
-function constatdesc_matches (item){
 
-    if(item.constascode == 1){
+function constatdesc_matches(item) {
+
+    if (item.constascode == 1) {
         return 'progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%;font-size: 10px;line-height: 15px;'
     }
-    if(item.constascode == 2){
+    if (item.constascode == 2) {
         return 'progress-bar progress-bar-warning progress-bar-striped active role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%;font-size: 10px;line-height: 15px;'
     }
-    if(item.constascode == 3){
+    if (item.constascode == 3) {
         return 'progress-bar progress-bar-success progress-bar-striped activ" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%;font-size: 10px;line-height: 15px;'
     }
 }
 
-function matches_score_type_team1(item){
-    if(item.constascode == 1){
+function matches_score_type_team1(item) {
+    if (item.constascode == 1) {
         return 'game-result__score-result';
     }
-    if(item.constascode == 3 || item.constascode == 2){
-        if(item.toufixsscr1 > item.toufixsscr2){
+    if (item.constascode == 3 || item.constascode == 2) {
+        if (item.toufixsscr1 > item.toufixsscr2) {
             return 'game-result__score-result game-result__score-result--winner';
-        }else if(item.toufixsscr1 < item.toufixsscr2){
+        } else if (item.toufixsscr1 < item.toufixsscr2) {
             return 'game-result__score-result game-result__score-result--loser--winner';
-        }else {
+        } else {
             return 'game-result__score-result';
         }
     }
 }
-function matches_score_type_team2(item){
-    if(item.constascode == 1){
+
+function matches_score_type_team2(item) {
+    if (item.constascode == 1) {
         return 'game-result__score-result';
     }
-    if(item.constascode == 3 || item.constascode == 2){
-        if(item.toufixsscr2 > item.toufixsscr1){
+    if (item.constascode == 3 || item.constascode == 2) {
+        if (item.toufixsscr2 > item.toufixsscr1) {
             return 'game-result__score-result game-result__score-result--winner--loser';
-        }else if(item.toufixsscr2 < item.toufixsscr1){
+        } else if (item.toufixsscr2 < item.toufixsscr1) {
             return 'game-result__score-result game-result__score-result--winner--loser';
-        }else {
+        } else {
             return 'game-result__score-result';
         }
     }
--- resources/views/matches.blade.php
+++ resources/views/matches.blade.php
@@ -13,7 +13,23 @@
             </div>
             <div class="widget__content card__content">
                 @foreach($listaTouinf as $objTorneosUsersMenu)
-
+                @if ($loop->first)
+                <ul id="post-torneos" class="posts posts--simple-list">
+                    <li data-touinfscode="{{$objTorneosUsersMenu->touinfscode}}" class="posts__item  post__items__custom post__items__custom__selected" 
+                        onclick="load_matches({{$objTorneosUsersMenu->touinfscode}}, null)">
+                        <figure class="posts__thumb">
+                                <img class="post__items__custom__img post__items__custom__img__selected" src="/images/{{ $objTorneosUsersMenu->touinfvlogt }}" alt="">
+                        </figure>
+                        <div class="posts__inner">
+                            <div class="posts__cat">
+                                <span class="label posts__cat-label"
+                                    style="font-size: 10px;background-color: #38a9ff">{{$objTorneosUsersMenu->touinfdstat}}</span>
+                            </div>
+                            <h6 class="posts__title">{{ $objTorneosUsersMenu->touinftname }}</h6>
+                        </div>
+                    </li>
+                </ul>
+                @else
                 <ul class="posts posts--simple-list">
                     <li  class="posts__item  post__items__custom" onclick="load_matches({{$objTorneosUsersMenu->touinfscode}},this)">
                         <figure class="posts__thumb">
@@ -28,6 +44,7 @@
                         </div>
                     </li>
                 </ul>
+                @endif
                 @endforeach
             </div>
         </aside>

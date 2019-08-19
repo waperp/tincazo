b797bd972029d59dd6142d7b690e80b2dd20df01
Miguel Angel <miguelwisse@wisse.tech>
2019-08-19 14:52:36 -0400

    up

--- resources/views/partials/layout/table_day.blade.php
+++ resources/views/partials/layout/table_day.blade.php
@@ -1,39 +1,37 @@
 <aside class="widget widget--sidebar card widget-popular-posts">
-        <div class="widget__title card__header" style="padding: 12px 24px">
-            <h4>TABLA POR DIA</h4>
-        </div>
-        <div class="row">
-          
-        </div>
-        
-        <div class="widget__content card__content p-2">
-                        <div style="width: auto" class="input-group input-group-sm date mt-1 mb-1" id="datetimepicker-filtrer-posiciones">
-                                <input class="form-control" id="date-filtrer-posiciones"
-                                    placeholder="Ingrese una fecha" required="" type="text" />
-                                <div class="input-group-addon input-group-append">
-                                    <div class="input-group-text">
-                                        <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-                                    </div>
-                                </div>
-                            </div>
-            <div class="table-responsive">
-
-                <table style="width: 100%" class="table table-hover table-standings"
-                    id="table-pociciones-dia" style="width: 100%">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>
-                                JUGADOR
-                            </th>
-                            <th>
-                                PTS
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
+    <div class="widget__title card__header" style="padding: 12px 24px">
+        <h4>TABLA POR DIA</h4>
+    </div>
+    <div class="row">
+    </div>
+    <div class="widget__content card__content p-2">
+        <div style="width: auto" class="input-group input-group-sm date mt-1 mb-1"
+            id="datetimepicker-filtrer-posiciones">
+            <input class="form-control" id="date-filtrer-posiciones" placeholder="Ingrese una fecha" required=""
+                type="text" />
+            <div class="input-group-addon input-group-append">
+                <div class="input-group-text">
+                    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                </div>
             </div>
         </div>
-    </aside>
+        <div class="table-responsive">
+            <table style="width: 100%" class="table table-hover table-standings" id="table-pociciones-dia"
+                style="width: 100%">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>
+                            JUGADOR
+                        </th>
+                        <th>
+                            PTS
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</aside>

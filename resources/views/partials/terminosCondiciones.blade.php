d85d1bca96d223bf2767d000dd305c0de0eecb30
Miguel Angel <miguelwisse@wisse.tech>
2019-09-12 12:33:09 -0400

    up

--- resources/views/partials/terminosCondiciones.blade.php
+++ resources/views/partials/terminosCondiciones.blade.php
@@ -1,139 +1,152 @@
-<!DOCTYPE html>
-<html lang="zxx">
+@extends('layout.welcome_test')
+@section('header')
+{{-- @include('partials.layout.header') --}}
+@endsection
+{{-- @section('slider')
+@include('partials.layout.slider')
+@endsection --}}
+@section('content')
+<div class="site-content">
+    <div class="container">
+        <div class="row">
+            <div class="content col-md-8 offset-md-2">
+                <!-- Article -->
+                <article class="card card--lg post post--single">
+                    <div class="card__content">
+                        <div  class="text-justify" class="post__content">
+                            <h2 class="text-center">TERMINOS Y CONDICIONES</h2>
+                            <p><strong>TINCAZO.COM</strong> es un producto de la empresa Dozure SRL y el acceso al sitio web y su
+                                utilización está sujeto a la aceptación y cumplimiento de los términos y
+                                condiciones que se exponen a continuación:</p>
+                            <ol>
+                                <li class="font-weight-bold text-dark" value="1"><strong> RESPONSABILIDAD</strong></li>
+                                <p>Tincazo.com se reserva el derecho para modificar en forma unilateral, sin
+                                    mediar previo aviso, ni comunicación al <strong>USUARIO y/o VISITANTE</strong>, estos
+                                    Términos y Condiciones, el diseño, la presentación o su configuración, los
+                                    servicios ofrecidos, los requisitos de registro o de utilización del sitio,
+                                    sin que ello genere derecho a reclamo o indemnización alguna en favor del
+                                    <strong>USUARIO y/o VISITANTE</strong>.</p>
+                                <p>Al acceder y utilizar este servicio y/o herramientas digitales existentes en
+                                    el presente sitio, usted acepta y accede a estar obligado por los términos y
+                                    condiciones de este acuerdo. <strong>TINCAZO.COM</strong> no se responsabilizara por la
+                                    existencia, actualizacion, veracidad, privacidad, funcionamiento,
+                                    modificaciones, contenidos, ofertas y legalidad de los sitios de terceros
+                                    vinculados a través de este sitio.</p>
+                                <p>El <strong>USUARIO y/o VISITANTE</strong> no podrá remover, eliminar, aumentar, añadir, ni
+                                    modificar total o parcialmente el Contenido, tampoco podrá volcar términos o
+                                    utilizar expresiones injuriosas, intimidatorias, calumniantes o contrarias a
+                                    las buenas costumbres. No podrá transmitir información o material que pueda,
+                                    concreta o eventualmente, violar derechos de un tercero o que contenga virus
+                                    o cualquier otro componente dañino.</p>
+                                <p><strong>TINCAZO.COM</strong> podra suspender transitoriamente o finalizar la publicacion de
+                                    la pagina sin previo aviso y en cualquier momento, sin que ello genere
+                                    derecho a indemnización alguna en favor del <strong>USUARIO y/o VISITANTE</strong>
+                                </p>
+                                <p>El <strong>USUARIO y/o VISITANTE</strong> reconoce y acepta que ni <strong>TINCAZO.COM</strong>, ni los
+                                    directores, empleados o representantes de cualquiera de ellos, es
+                                    responsable por daños que surjan de o resulten del uso de esta página,
+                                    incluyendo cualquier error, omisión, interrupción, falla, eliminación de
+                                    archivos o correos electrónicos (e-mails), defectos, virus, y/o demoras en
+                                    la operación o transmisión y/o de cualquier otro tipo.</p>
+                                <P>
+                                    El sitio web puede ser utilizado por el <strong>USUARIO y/o VISITANTE</strong> en forma
+                                    totalmente libre y gratuita.
+                                </P>
+                                <li class="font-weight-bold text-dark"><strong> POLÍTICA DE PRIVACIDAD</strong> </li>
+                                <p>La utilización e ingreso a la presente página web de <strong>TINCAZO.COM</strong>, será
+                                    considerado como aceptación de los términos de esta Política de Privacidad
+                                    por parte del <strong>USUARIO y/o VISITANTE</strong>.</p>
+                                <p>Los datos personales que el <strong>USUARIO y/o VISITANTE</strong> brinde libre y
+                                    voluntariamente a <strong>TINCAZO.COM</strong>, tales como nombres, correo electrónico,
+                                    teléfono, y/o cualquier otro que voluntariamente suministre a <strong>TINCAZO.COM</strong>
+                                    cuando ello sea necesario para brindarle un servicio específico, son
+                                    incluidos en archivos automatizados, procesados bajo normas de estricta
+                                    confidencialidad y protección de datos.</p>
+                                <p>
+                                    <strong>TINCAZO.COM</strong> tiene el derecho de inhabilitar cualquier cuenta de
+                                    identificación de USUARIO o contraseña si, haciendo un análisis razonable,
+                                    ha incumplido cualquier de las disposiciones de estas Condiciones.
+                                </p>
+                                <li class="font-weight-bold text-dark"><strong> SITIOS ENLAZADOS </strong> </li>
+                                <p>A través de la presente página se pone a su disposición dispositivos técnicos de
+                                    enlace (tales como, entre otros, links, banners, botones), directorios y
+                                    herramientas de búsqueda que les permiten acceder a páginas web pertenecientes a
+                                    terceros (en adelante los “SITIOS ENLAZADOS”). La instalación de estos enlaces en
+                                    las páginas de Tincazo.com se limita a facilitar a los <strong>USUARIO y/o VISITANTE</strong>, la
+                                    búsqueda y acceso, a la información disponible de los sitios enlazados en Internet,
+                                    y no presupone que existe ninguna clase de vínculo o asociación entre Tincazo.com,
+                                    sus subsidiarias o afiliadas y los operadores de los sitios enlazados. Tincazo.com
+                                    no controla, ni hacen propios los servicios, información, datos, archivos, productos
+                                    y cualquier clase de material existente en los sitios enlazados. Por lo tanto, el
+                                    <strong>USUARIO y/o VISITANTE</strong>, debe extremar la prudencia en la valoración y utilización de
+                                    los servicios, información, datos, archivos, productos y cualquier clase de material
+                                    existente en los sitios enlazados. </p>
+                                <p><strong>TINCAZO.COM</strong> no garantiza ni asume responsabilidad alguna por los daños y perjuicios
+                                    que de cualquier naturaleza pueda causarse por:
+                                    <ul>
+                                        <li type="disc">El funcionamiento, disponibilidad, accesibilidad o continuidad
+                                            de sitios enlazados.</li>
+                                        <li type="disc">El mantenimiento de los servicios, información, datos, archivos,
+                                            productos y cualquier clase de material existente en los sitios enlazados.
+                                        </li>
+                                        <li type="disc">Las obligaciones y ofertas existentes en los sitios enlazados.
+                                        </li>
+                                    </ul>
+                                </p>
+                                <ol>
+                                    <li class="font-weight-bold text-dark" value="3"><span>1 </span>CALIDAD</li>
+                                    <p>Mediante el uso de esta página, el <strong>USUARIO y/o VISITANTE</strong> reconoce y
+                                        acepta que <strong>TINCAZO.COM</strong> no controla ni garantiza la ausencia de virus en
+                                        el contenido de los sitios enlazados, ni la ausencia de otros elementos
+                                        que pudieran producir alteraciones en sus sistemas informáticos o en los
+                                        documentos electrónicos y archivos almacenados en los archivos
+                                        informáticos del USUARIO. </p>
+                                    <li class="font-weight-bold text-dark" value="3"><span>2 </span>CALIDAD</li>
+                                    <p>Mediante el uso de esta página, el <strong>USUARIO y/o VISITANTE</strong> reconoce y acepta que
+                                        <strong>TINCAZO.COM</strong> queda excluida de cualquier responsabilidad que pudiera ser causada
+                                        por el uso no autorizado de las marcas u otros derechos de propiedad intelectual
+                                        de terceros o contenidos en los sitios enlazados. </p>
+                                    <p>De igual manera, las eventuales referencias que se hagan en esta página a
+                                        cualquier producto, servicio, proceso, sitio enlazado, hipertexto o cualquier
+                                        otra información en la que se utilicen marcas, signos distintivos o dominios, el
+                                        nombre comercial o el nombre del fabricante, suministrador, etc., que sean
+                                        titularidad de terceros, en ningún momento constituirán, ni implicarán respaldo
+                                        o recomendación alguna por parte de <strong>TINCAZO.COM</strong>. Y en ningún caso, <strong>TINCAZO.COM</strong>
+                                        se asignará propiedad ni responsabilidad sobre los mismos.</p>
+                                </ol>
 
-<head>
-    <!-- Basic Page Needs
-	================================================== -->
-    <title>TINCAZO</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Sports Club, League and News HTML Template">
-    <meta name="author" content="Dan Fisher">
-    <meta name="keywords" content="sports club news HTML template">
-    <meta content="{{ csrf_token() }}" name="_token" />
-    <!-- Favicons
-	================================================== -->
-    <link rel="shortcut icon" href="/favicon.ico">
-    {{-- <link rel="apple-touch-icon" sizes="120x120" href="/favicon-120.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/favicon-152.png"> --}}
-    <!-- Mobile Specific Metas
-	================================================== -->
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
-    <!-- Google Web Fonts
-	================================================== -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CSource+Sans+Pro:400,700" rel="stylesheet">
-    <!-- CSS
-	================================================== -->
-    <!-- Vendor CSS -->
-    <link href="/css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/fonts/font-awesome/css/all.min.css" rel="stylesheet">
-    <link href="assets/fonts/font-awesome/css/v4-shims.min.css" rel="stylesheet">
-    <link href="assets/fonts/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
-    <link href="assets/vendor/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
-    <link href="assets/vendor/slick/slick.css" rel="stylesheet">
-    <!-- Template CSS-->
-    <link href="/css/select2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/select2-bootstrap4.min.css">
+                                <li class="font-weight-bold text-dark"><strong> TERMINACIÓN DEL ACCESO </strong> </li>
+                                <p><strong>TINCAZO.COM</strong> podrá, en cualquier momento, terminar o suspender el acceso que el
+                                    <strong>USUARIO y/o VISITANTE</strong> tenga a todo o parte de este sitio, sin aviso previo, y sin
+                                    que ello genere derecho a reclamo o indemnización alguna. Ni la terminación o
+                                    suspensión del acceso, ni cualquier acción o inacción del <strong>USUARIO y/o VISITANTE</strong>,
+                                    terminará las disposiciones de estos Términos y Condiciones, los que permanecerán en
+                                    plena fuerza y vigor de forma indefinida, sujetas sólo a cualquier cambio que
+                                    <strong>TINCAZO.COM</strong> efectúe. </p>
 
-
-    <link href="assets/css/style-soccer.css" rel="stylesheet">
-
-    <!-- Custom CSS-->
-    <link href="assets/css/custom.css?css={{ time() }}" rel="stylesheet">
-
-
-</head>
-
-<body data-template="template-soccer">
-    <input id="token" name="_token" type="hidden" value="{{ csrf_token() }}" />
-    <input type="hidden" id="fecha-actual-server" value="{{ Carbon\Carbon::now()->toDateString() }}" />
-    <div class="site-wrapper clearfix">
-        <div class="site-overlay"></div>
-        <!-- Header
-		================================================== -->
-        <!-- Header Mobile -->
-        <div class="header-mobile clearfix" id="header-mobile">
-            <div class="header-mobile__logo">
-                <a href="_soccer_index.html"><img src="assets/images/soccer/logo.png"
-                        srcset="assets/images/soccer/logo@2x.png 2x" alt="Alchemists"
-                        class="header-mobile__logo-img"></a>
+                                <li class="font-weight-bold text-dark"><strong> VIOLACIONES DEL SISTEMA O BASES DE
+                                        DATOS</strong> </li>
+                                <p>Es ilícita cualquier acción o uso de dispositivos, software, u otros instrumentos
+                                    tendientes a interferir tanto en las actividades y operatoria de Tincazo.com, así
+                                    como en las ofertas, descripciones, cuentas o bases de datos de <strong>TINCAZO.COM</strong>.
+                                    Cualquier intromisión, tentativa o actividad violatoria o contraria a las leyes
+                                    sobre derechos de propiedad intelectual, seguridad de los sistemas, y/o a las
+                                    prohibiciones estipuladas en este documento harán pasible a su responsable de las
+                                    acciones legales pertinentes, y a las sanciones previstas por este acuerdo.</p>
+                                <li style="list-style-type: none;" class="font-weight-bold text-dark">
+                                    <strong>CONTACTENOS</strong> </li>
+                                <p>Si necesita comunicarse con Tincazo.com, por favor envie un correo electronico : <br>
+                                    <a href="mailto:soporte@tincazo.com">soporte@tincazo.com</a>
+                                    <br>
+                                    Gracias por visitar TINCAZO.COM
+                                </p>
+                            </ol>
+                        </div>
+                </article>
+                <!-- Article / End -->
             </div>
-            <div class="header-mobile__inner">
-                <a id="header-mobile__toggle" class="burger-menu-icon"><span class="burger-menu-icon__line"></span></a>
-                <span class="header-mobile__search-icon" id="header-mobile__search-icon"></span>
-            </div>
-        </div>
-        <!-- Header Desktop -->
-
-        <!-- Header / End -->
-        <!-- Pushy Panel - Dark -->
-
-        <!-- Pushy Panel - Dark / End -->
-
-        <!-- Hero Slider
-		================================================== -->
-
-        <!-- Content
-        ================================================== -->
-        <div class="site-content">
-            <div class="container">
-                <div class="row">
-                    <div class="content col-md-8 offset-md-2">
-                        <!-- Article -->
-                        <article class="card card--lg post post--single">
-                            <div class="card__content">
-                                <div class="post__content">
-                                </div>
-                        </article>
-                        <!-- Article / End -->
-                    </div>
-                    <!-- Content / End -->
-                </div>
-            </div>
+            <!-- Content / End -->
         </div>
     </div>
-    <!-- Content / End -->
-    <!-- Footer
-		================================================== -->
-
-    <!-- Footer / End -->
-    <!-- Login/Register Tabs Modal -->
-
-    <!-- Login/Register Tabs Modal / End -->
-    {{-- @include('partials.partialModal') --}}
-    <!-- Javascript Files
-	================================================== -->
-    <!-- Core JS -->
-    <script src="assets/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/vendor/jquery/jquery-migrate.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/core.js"></script>
-    <!-- Vendor JS -->
-    <script src="assets/vendor/twitter/jquery.twitter.js"></script>
-    <!-- Template JS -->
-    <script src="assets/js/init.js"></script>
-    <script src="assets/js/custom.js"></script>
-    <script src="/js/jquery.uploadPreview.js">
-    </script>
-    <script src="/js/transition.js" type="text/javascript">
-    </script>
-    <script src="/js/collapse.js" type="text/javascript">
-    </script>
-    <script src="/js/moment.js" type="text/javascript">
-    </script>
-    <script src="/js/moment-with-locales.js" type="text/javascript">
-    </script>
-    <script type="text/javascript">
-    </script>
-    <script src="/assets/vendor/jpreloader2/js/jpreloader.js" type="text/javascript">
-    </script>
-    <script src="/js/sum.js" type="text/javascript"></script>
-    <script src="/js/matches.js?v={{ time() }}" type="text/javascript">
-    </script>
-    <script src="/js/jquery.Wload.js"></script>
-    <script src="/js/loading-bar.js"></script>
-    <script src="/js/progressbar.min.js"></script>
-</body>
-
-</html>
+</div>
+@endsection

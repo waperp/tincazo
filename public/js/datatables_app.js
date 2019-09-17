500b019f6e56a43d3af8be87c9d1b04b80e3371e
Miguel Angel <miguelwisse@wisse.tech>
2019-09-17 12:50:59 -0400

    Update datatables_app.js

--- public/js/datatables_app.js
+++ public/js/datatables_app.js
@@ -763,10 +763,8 @@ var table = $("#table-admin-equipo").DataTable({
     }]
 });
 var table = $("#table-admin-torneo-equipo").DataTable({
-    colReorder: true,
-    order: [
-        [1, 'desc']
-    ],
+    // colReorder: true,
+    
     dom: "<'row'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-6'f>>" +
         "<'row'<'col-sm-12'tr>>" +
         "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
